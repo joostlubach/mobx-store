f96c60657dd05dc702fe9fd6140825d65eb233e3
Joost Lubach <joostlubach@gmail.com>
2024-08-05 10:22:59 +0200

    Initialization optimization.

--- src/lifecycle.ts
+++ src/lifecycle.ts
@@ -6,7 +6,7 @@ import { Store } from './types'
 export async function initStore(store: Store) {
   try {
     const meta = metaFor(store, false)
-    if (meta == null) { return }
+    if (meta == null) { return true }
 
     for (const key of meta.inits) {
       const fn = (store as any)[key]
@@ -20,8 +20,7 @@ export async function initStore(store: Store) {
     config.logger.debug(`Initialized ${store.constructor.name}`)
     return true
   } catch (error) {
-    config.logger.error(`Error while initializing ${store.constructor.name}`)
-    config.logger.error(error)
+    config.logger.error(`Error while initializing ${store.constructor.name}`, [error])
     return false
   }
 }
@@ -29,7 +28,7 @@ export async function initStore(store: Store) {
 export async function deinitStore(store: Store) {
   try {
     const meta = metaFor(store, false)
-    if (meta == null) { return }
+    if (meta == null) { return true }
 
     for (const deinit of meta.deinits) {
       const fn = isFunction(deinit) ? deinit : (store as any)[deinit]
@@ -45,27 +44,46 @@ export async function deinitStore(store: Store) {
   }
 }
 
-export function initStores(stores: Store[], timeout: number = 5000): Promise<boolean> {
-  const promises = stores.flatMap(store => Promise.race([
-    initStore(store),
-    timeoutAfter(`Init of ${store.constructor.name} timed out`, timeout),
-  ]))
-  return Promise.all(promises).then(results => results.every(it => it))
+export async function initStores(stores: Store[], timeout: number = 5000): Promise<boolean> {
+  const promises = stores.map(store => runAsyncWithTimeout(
+    () => initStore(store),
+    timeout,
+    `Init of ${store.constructor.name} timed out`
+  ))
+
+  const results = await Promise.all(promises)
+  return results.every(it => it)
 }
 
-export function deinitStores(stores: Store[], timeout: number = 5000): Promise<boolean> {
-  const promises = stores.flatMap(store => Promise.race([
-    deinitStore(store),
-    timeoutAfter(`Deinit of ${store.constructor.name} timed out`, timeout),
-  ]))
-  return Promise.all(promises).then(results => results.every(it => it))
+export async function deinitStores(stores: Store[], timeout: number = 5000): Promise<boolean> {
+  const promises = stores.map(store => runAsyncWithTimeout(
+    () => deinitStore(store),
+    timeout,
+    `Deinit of ${store.constructor.name} timed out`
+  ))
+
+  const results = await Promise.all(promises)
+  return results.every(it => it)
 }
 
-function timeoutAfter(message: string, ms: number) {
-  return new Promise((_, reject) => {
-    setTimeout(() => {
+function runAsyncWithTimeout(fn: () => Promise<any>, timeout: number, message: string): Promise<boolean> {
+  return new Promise((resolve, reject) => {
+    let resolved: boolean = false
+
+    const onSuccess = () => {
+      if (resolved) { return }
+      resolved = true
+      resolve(true)
+    }
+
+    const onTimeout = () => {
+      if (resolved) { return }
       config.logger.error(message)
-      reject(false)
-    }, ms)
+      resolved = true
+      resolve(false)
+    }
+
+    setTimeout(onTimeout, timeout)
+    fn().then(onSuccess).catch(reject)  
   })
 }
